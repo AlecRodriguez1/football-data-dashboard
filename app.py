7e50a238429c2c2db133ea67bb4e1d69affd19f3
AlecRodriguez1 <alecrv321@gmail.com>
2025-12-22 09:54:08 -0400

    Implemented league-based top 25 scorers table

--- app.py
+++ app.py
@@ -6,9 +6,37 @@ def main():
     df = pd.read_csv("data/player_stats_25.csv")
     
     st.title("Football Data Dashboard 2024-2025")
-    st.subheader("Dataset preview")
-    st.dataframe(df.head(20), use_container_width=True)
+    
+#---------------League Selector---------------------
 
+    leagues = (df["Comp"].unique())
+    selected_league = st.selectbox(
+        "Select a league",
+        leagues
+    )
+
+#--------------Filtering Data for top scorers--------------
+
+    filtered_league = df[df["Comp"] == selected_league]
+    
+    top_scorers_data = ["Player", "MP", "Gls", "Squad", "Pos", "Age"]
+    filtered_league_df = filtered_league[top_scorers_data]
+
+    top_scorers = (
+        filtered_league_df
+        .sort_values("Gls", ascending=False)
+        .reset_index(drop=True)
+        )
+    
+    top_scorers["RK"] = top_scorers.index + 1
+    display_cols = ["RK"] + top_scorers_data
+
+    st.subheader(f"Top 25 scorers in {selected_league}")
+    st.dataframe(
+        top_scorers[display_cols].head(25), 
+        use_container_width=True,
+        hide_index=True
+        )
 
 if __name__ == "__main__":
     main()
