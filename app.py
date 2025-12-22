94dd8ce65eea96c348b84b6d0e7dbeab9778456e
AlecRodriguez1 <alecrv321@gmail.com>
2025-12-22 19:33:37 -0400

    Improved code structure using helper function

--- app.py
+++ app.py
@@ -2,6 +2,19 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+def show_rank_table(df, sort_col, display_cols, title, top_n=25):
+    rank = (
+        df.sort_values(sort_col, ascending=False)
+        .reset_index(drop=True)
+    )
+    rank["RK"] = rank.index + 1
+
+    st.subheader(title)
+    st.dataframe(
+        rank[display_cols].head(top_n),
+        use_container_width=True,
+        hide_index=True
+    )
 
 def main():
     df = pd.read_csv("data/player_stats_25.csv")
@@ -16,27 +29,20 @@ def main():
 
 #------------Top scorers across all Top 5 leagues---------------
 
-    five_league_scorers = (
-        df[top_scorers_all_cols]
-        .sort_values("Gls", ascending=False)
-        .reset_index(drop=True)
-        )
-
-    five_league_scorers["RK"] = five_league_scorers.index + 1
     disp_scorers = ["RK"] + top_scorers_all_cols
 
-    st.subheader("Top 25 scorers across top 5 leagues")
-    st.dataframe(
-        five_league_scorers[disp_scorers].head(25),
-        use_container_width=True,
-        hide_index=True
+    show_rank_table(df[top_scorers_all_cols], 
+        sort_col="Gls", 
+        display_cols=disp_scorers, 
+        title="Top 25 scorers across top 5 leagues", 
+        top_n=25
     )
 
 #-----------------Graph---------------------------------
 
     st.subheader("Top 10 scorers in top 5 leagues ")
     
-    top10_all = five_league_scorers.head(10)
+    top10_all = df[top_scorers_all_cols].sort_values("Gls", ascending=False).head(10)
     
     fig1 = px.bar(top10_all, x="Player", y="Gls", color = "Player", title="Top 10 scorers")
     st.plotly_chart(fig1)
@@ -44,41 +50,33 @@ def main():
 
 #---------------League Selector---------------------
 
-    leagues = (df["Comp"].unique())
+    leagues = sorted(df["Comp"].unique())
     selected_league = st.selectbox(
         "Select a league",
         leagues
     )
 
-#--------------Top scorers for selected leagues--------------
+#--------------Top 25 scorers for selected leagues--------------
 
     filtered_league = df[df["Comp"] == selected_league]
-    
     filtered_league_df = filtered_league[top_scorers_by_league_cols]
-
-    top_scorers = (
-        filtered_league_df
-        .sort_values("Gls", ascending=False)
-        .reset_index(drop=True)
-        )
     
-    top_scorers["RK"] = top_scorers.index + 1
     display_cols = ["RK"] + top_scorers_by_league_cols
 
-    st.subheader(f"Top 25 scorers in {selected_league}")
-    st.dataframe(
-        top_scorers[display_cols].head(25), 
-        use_container_width=True,
-        hide_index=True
-        )
+    show_rank_table(filtered_league_df, 
+        sort_col="Gls", 
+        display_cols=display_cols, 
+        title=f"Top 25 scorers in {selected_league}", 
+        top_n=25
+    )
 
 #-----------------Graph---------------------------------
 
     st.subheader(f"Top 10 scorers in {selected_league} ")
     
-    top10 = top_scorers.head(10)
+    top_10 = filtered_league_df.sort_values("Gls", ascending=False).head(10)
     
-    fig2 = px.bar(top10, x="Player", y="Gls", color = "Player", title="Top 10 scorers")
+    fig2 = px.bar(top_10, x="Player", y="Gls", color = "Player", title="Top 10 scorers")
     st.plotly_chart(fig2)
     st.divider()
 
